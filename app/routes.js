adf335e4e4452c946686831bb9d66e76ea125a3d
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2016-02-23 09:52:01 +0000

    Some general updates to the kit.

--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,53 @@
-var express = require('express');
-var router = express.Router();
+var express   = require('express'),
+    fs        = require('fs'),
+    CSON      = require('cson'),
+    router    = express.Router();
 
 router.get('/', function (req, res) {
   res.render('index');
 });
 
+function grabCSON(file)
+{
+  var data = {};
+  try {
+    CSON.parseCSONFile(file,function(err,results)
+    {
+      if (results) data = results;
+      if (err) data.error = "Error! Looks like there was a problem with the data.\n"+err;
+    });
+  } catch(e) {
+    data.error = "Error! Looks like there was a problem finding the data file.\n"+e;
+  }
+  return data;
+}
 
-// Example route: Passing data into a page
-router.get('/examples/template-data', function (req, res) {
-  res.render('examples/template-data', { 'name' : 'Foo' });
+// add your routes here
+router.get('/role/:name?', function (req, res, next)
+{
+  var name = req.params.name;
+  if (!name) name = 'tom';
+
+  req.data = {};
+  req.data.general = grabCSON(__dirname + '/data/general.cson');
+  req.data.user = grabCSON(__dirname + '/data/users/'+name+'.cson');
+
+  req.url = '/role/';
+  next();
 });
 
 // add your routes here
+router.get('/location/:name?', function (req, res, next)
+{
+  var name = req.params.name;
+  if (!name) name = 'leedsone';
+
+  req.data = {};
+  req.data.general = grabCSON(__dirname + '/data/general.cson');
+  req.data.location = grabCSON(__dirname + '/data/locations/'+name+'.cson');
+
+  req.url = '/location/';
+  next();
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,98 +1,129 @@
-var path = require('path'),
-    express = require('express'),
-    nunjucks = require('express-nunjucks'),
-    routes = require(__dirname + '/app/routes.js'),
-    favicon = require('serve-favicon'),
-    app = express(),
-    basicAuth = require('basic-auth'),
-    bodyParser = require('body-parser'),
-    config = require(__dirname + '/app/config.js'),
-    port = (process.env.PORT || config.port),
-    utils = require(__dirname + '/lib/utils.js'),
+var path          = require('path'),
+    express       = require('express'),
+    browserSync   = require('browser-sync'),
+    nunjucks      = require('express-nunjucks'),
+    favicon       = require('serve-favicon'),
+    // basicAuth     = require('basic-auth-connect'),
+    // bodyParser    = require('body-parser'),
+    // cookieParser  = require('cookie-parser'),
+    port          = (process.env.PORT || 3000),
+    app           = express(),
 
-// Grab environment variables specified in Procfile or as Heroku config vars
+    // routing and extras
+    routes        = require(__dirname + '/lib/default-routes.js'),
+    app_routes    = require(__dirname + '/app/routes.js'),
+    // staff_routes  = require(__dirname + '/app/views/staffui/routes.js'),
+    // fdbck_routes  = require(__dirname + '/app/views/feedback/routes.js'),
+    // user_data     = require(__dirname + '/lib/user_data.js'),
+
+    // Grab environment variables specified in Procfile or as Heroku config vars
     username = process.env.USERNAME,
     password = process.env.PASSWORD,
-    env      = process.env.NODE_ENV || 'development',
-    useAuth  = process.env.USE_AUTH || config.useAuth;
+    env = process.env.NODE_ENV || 'development';
 
-// Authenticate against the environment-provided credentials if running
-// the app in production (Heroku, effectively)
-if (env === 'production' && useAuth === 'true'){
-    app.use(utils.basicAuth(username, password));
-}
+/*
+  Authenticate against the environment-provided credentials,
+  if running the app in production (Heroku, effectively)
+*/
+// if (env === 'production') {
+//   if (!username || !password) {
+//     console.log('Username or password is not set, exiting.');
+//     process.exit(1);
+//   }
+//   app.use(express.basicAuth(username, password));
+// }
 
-// Application settings
+
+/*
+  Setting up the templating system, nunjucks etc.
+*/
 app.set('view engine', 'html');
-app.set('views', [__dirname + '/app/views', __dirname + '/lib/']);
-
+app.set('views', [__dirname + '/app/views/', __dirname + '/lib/']);
 nunjucks.setup({
-  autoescape: true,
-  watch: true,
-  noCache: true
-}, app);
+    autoescape: true,
+    watch: true,
+    noCache: true,
+}, app, function(env)
+{
+  env.addFilter('slugify', function(str) {
+      return str.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()’]/g,"").replace(/ +/g,'_').toLowerCase();
+  });
+  env.addFilter('check', function(str,checkee,output) {
+      return str == checkee ? output : '';
+  });
+  env.addFilter('classify', function(str,checkee,output) {
+      return str.split(' ')[0].toLowerCase();
+  });
+  env.addFilter('sanssuffix', function(str) {
+      return str.replace(".html","");
+  });
+});
 
-// Middleware to serve static assets
+
+/*
+  Middleware to serve static assets.
+*/
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/public', express.static(__dirname + '/govuk_modules/govuk_template/assets'));
 app.use('/public', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit'));
 app.use('/public/images/icons', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit/images'));
-
-// Elements refers to icon folder instead of images folder
 app.use(favicon(path.join(__dirname, 'govuk_modules', 'govuk_template', 'assets', 'images','favicon.ico')));
 
-// Support for parsing data in POSTs
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 
-// send assetPath to all views
+/*
+  Support for parsing data in POSTs.
+*/
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:true}));
+// app.use(cookieParser());
+// app.use(user_data.form_to_cookie());
+
+
+/*
+  Send assetPath to all views.
+*/
 app.use(function (req, res, next) {
   res.locals.asset_path="/public/";
   next();
 });
 
-// Add variables that are available in all views
-app.use(function (req, res, next) {
-  res.locals.serviceName=config.serviceName;
-  next();
-});
 
-// routes (found in app/routes.js)
-if (typeof(routes) != "function"){
+/*
+  Routers
+*/
+if (typeof(routes) != "function") {
   console.log(routes.bind);
   console.log("Warning: the use of bind in routes is deprecated - please check the prototype kit documentation for writing routes.")
   routes.bind(app);
 } else {
-  app.use("/", routes);
+  console.log('Using routes');
+  app.use(app_routes);    // these have to come first.
+  // app.use(staff_routes);  // these have to come first.
+  // app.use(fdbck_routes);  // these have to come first.
+  app.use(routes);        // these come last because they mop up!
 }
 
-// auto render any view that exists
-app.get(/^\/([^.]+)$/, function (req, res) {
 
-  var path = (req.params[0]);
-
-  res.render(path, function(err, html) {
-    if (err) {
-      res.render(path + "/index", function(err2, html) {
-        if (err2) {
-          console.log(err);
-          res.status(404).send(err + "<br>" + err2);
-        } else {
-          res.end(html);
-        }
-      });
-    } else {
-      res.end(html);
-    }
+/*
+  Start the app.
+  Use browserSync if we're in development.
+*/
+if (env === 'production') {
+  app.listen(port);
+} else {
+  app.listen(port,function()
+  {
+    browserSync({
+      proxy:'localhost:'+port,
+      port:port+1,
+      ui:false,
+      files:['public/**/*.{js,css,png}','app/views/**/*.html'],
+      ghostmode:{clicks:true, forms: true, scroll:true},
+      open:false,
+    });
   });
+}
 
-});
-
-// start the app
-
-app.listen(port);
 console.log('');
 console.log('Listening on port ' + port);
 console.log('');
